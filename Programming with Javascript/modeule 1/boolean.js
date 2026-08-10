af07aacaeb85bdb6d3850e20abc86ac677ab8bb1
Sami <aannss5602@gmail.com>
2026-08-10 12:21:51 +0500

    string and bollean added to js M1

---
+++ Programming with Javascript/modeule 1/boolean.js
@@ -0,0 +1,13 @@
+// True or False.
+
+console.log(1<2);   // True
+console.log(1>2);   // False
+
+console.log(1 == 2)  // False
+var score = 100;
+console.log(100 == "100") // True it will not check datatype
+console.log(100 === "100")  // False it will check datatype too
+
+console.log(1 != 1) // False
+console.log(1 !== "1") // True
+
---
+++ Programming with Javascript/modeule 1/string.js
@@ -0,0 +1,4 @@
+console.log('hello there!');
+console.log("hello there!123");
+// console.log('Its's a lovely day');     -> error
+console.log("It's a lovely day");
