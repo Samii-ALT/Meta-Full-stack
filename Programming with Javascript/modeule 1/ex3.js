efaaec96f4caaf460b505469cc0a73717d235bc8
Sami <aannss5602@gmail.com>
2026-08-10 17:16:29 +0500

    Exercise 3 added

---
+++ Programming with Javascript/modeule 1/ex3.js
@@ -0,0 +1,41 @@
+for(var i=1; i<=5; i++){
+    console.log(i);
+}
+console.log('Counting completed!');
+
+
+
+
+for(var i=5; i>0; i--){
+    console.log(i);
+}
+console.log("Contdown finished!")
+
+
+
+
+var i=1;
+while(i<6){
+    console.log(i);
+    i++;
+}
+console.log('Counting completed')
+
+
+
+
+var i=5;
+while(i>0){
+    console.log(i)
+    i--;
+}
+console.log("Counting completed")
+
+
+
+
+var i=18;
+while(i<23){
+    console.log("20" + i);
+    i++;
+}
