22eda6aff1f671dd057bc76fb6c254872d53f96d
Sami <aannss5602@gmail.com>
2026-08-10 15:42:44 +0500

    Two exercises and Condion.js added

---
+++ Programming with Javascript/modeule 1/condition.js
@@ -0,0 +1,44 @@
+// If else
+
+var result = 48;
+if(result>40){
+    console.log("Congratulation You are Passed");
+}else{
+    console.log("Unfortunately you couldn't pass the test");
+}
+
+
+
+// if elseif else
+
+var place = "First";
+if(place == "First"){
+    console.log("Gold")
+}
+else if(place == "Second"){
+    console.log("Silver")
+}
+else if(place == Third){
+    console.log("Bronze")
+}
+else{
+    console.log("No medal")
+}
+
+
+// Switch Statement
+
+var Place = "Fourth";
+switch(Place){
+    case 'First':
+        console.log("Gold");
+        break;
+    case 'Second':
+        console.log('Silver');
+        break;
+    case 'Third':
+        console.log('Bronze')
+        break;
+    default:
+        console.log("No medal")
+}
---
+++ Programming with Javascript/modeule 1/ex1.js
@@ -0,0 +1,18 @@
+var score = 8;
+console.log("Mid-level skills:",score>0 && score<10)
+
+
+
+var timeremaining = 0;
+var power = 10;
+console.log("Game over:", timeremaining==0 || power==0);
+
+
+var num1 = 2;
+var num2 = 5;
+var test1 = num1 % 2;
+var test2 = num2 % 2;
+var result1 = test1 == 0;
+var result2 = test2 == 0;
+console.log("Is", num1, "an even number?", result1);
+console.log("Is", num2, "an even number?", result2);
