fb61ca77ca861e2342d574e801f2293440e4d3f2
Sami <aannss5602@gmail.com>
2026-08-10 17:28:34 +0500

    Nested Loop added

---
+++ Programming with Javascript/modeule 1/nested_loop.js
@@ -0,0 +1,16 @@
+for(var week=1; week<=2; week++){
+    for(var day=1; day<=7; day++){
+        console.log("Week", week, "day", day);
+    }
+}
+
+
+
+
+
+for(var i=2023; i<=2024; i++){
+    console.log(i);
+    for(var j=6; j<=8; j++){
+        console.log(j);
+    }
+}
