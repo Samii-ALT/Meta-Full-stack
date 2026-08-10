961259845f0b2bd74a7ca5546d2d499a52309269
Sami <aannss5602@gmail.com>
2026-08-10 12:04:57 +0500

    JavaScript started

---
+++ Programming with Javascript/modeule 1/number.js
@@ -0,0 +1,10 @@
+console.log(123);
+console.log(123.456);
+console.log(2+2);
+console.log(4-2);
+console.log(4*4);
+console.log(16/4);
+console.log(10**2)  // 10 power 2 = 100
+console.log(16%8);
+console.log(2*4+8);
+console.log(2*(4+8));
