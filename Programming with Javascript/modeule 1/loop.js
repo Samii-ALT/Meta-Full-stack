f120e0046178dd144f0986e821959fb2dd6c3251
Sami <aannss5602@gmail.com>
2026-08-10 16:37:50 +0500

    loops added in JS M1

---
+++ Programming with Javascript/modeule 1/loop.js
@@ -0,0 +1,16 @@
+var i = 1;
+while(i<4){
+    console.log(i);
+    i++;
+}
+
+
+// for loop
+for(var i =1; i<=3; i++){
+    console.log(i);
+}
+console.log("Go!")
+
+for(var j=10; j>0; j--){
+    console.log(j)
+}
