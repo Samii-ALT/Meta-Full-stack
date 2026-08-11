c5e8dd8c92ad705e527ab80159bf4d6bb5b6d0e7
Sami <aannss5602@gmail.com>
2026-08-11 17:00:48 +0500

    INTRO to bugs and error+types added

---
+++ Programming with Javascript/modeule 2/bugs&errors.js
@@ -0,0 +1,12 @@
+function add(a, b){
+    console.log(a+b);
+}
+add("1", "2");   // This is a bug bcz program is still running but output is 12.
+
+
+console.log(c+d);  // Reference error c and d is not defined. Program will not run.
+
+var greet = "Hello; 
+// Syntax error bcz the " is not closed.
+
+5.pop() // This is Type error because there is no pop method for numbers.
