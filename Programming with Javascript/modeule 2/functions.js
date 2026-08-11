47e896145eea8035687c2fbee91a101950a37938
Sami <aannss5602@gmail.com>
2026-08-11 07:39:32 +0500

    Function file added in new folder modeule 2 in javascript

---
+++ Programming with Javascript/modeule 2/functions.js
@@ -0,0 +1,18 @@
+function addtwonum(){
+    var a = 12;
+    var b = 8;
+    var c = a+b;
+    console.log(c)
+}
+
+addtwonum();
+
+
+
+
+function Addtwonum(a, b){
+    var c = a+b;
+    console.log(c);
+}
+Addtwonum(2, 2);
+Addtwonum(37, 78);
