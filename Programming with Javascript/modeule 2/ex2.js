4927b385d77bebe31c72277a45d755cfad1f5d94
Sami <aannss5602@gmail.com>
2026-08-11 13:03:14 +0500

    Exercise 2 added in JS M2

---
+++ Programming with Javascript/modeule 2/ex2.js
@@ -0,0 +1,28 @@
+var clothes = []
+clothes.push("Shirt");
+clothes.push("Pants");
+clothes.push("Shawl");
+clothes.push("Coat");
+clothes.push("Hoodie");
+clothes.pop();
+clothes.push("Slippers");
+console.log(clothes);
+
+
+
+var favCar={
+    color: "Black",
+    convertible: true
+}
+
+console.log(favCar)
+
+
+
+// Q2
+
+
+var arr=[1, 2]
+var Newlength = arr.push(3);
+console.log(Newlength);
+console.log(arr)
