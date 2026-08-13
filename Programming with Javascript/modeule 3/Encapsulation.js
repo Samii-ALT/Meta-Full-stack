a7faac10cf8a47a87e18786974808f8f88df1a56
Sami <aannss5602@gmail.com>
2026-08-13 13:51:54 +0500

    JS MOD3: Encapsulation and abstraction added

---
+++ Programming with Javascript/modeule 3/Encapsulation.js
@@ -0,0 +1,8 @@
+// Encapsulation means that hide what the actual function looks from inside.
+
+// For example..
+console.log("abc".toUpperCase());
+
+// Java process "toUpperCase" function internally but we don't know how 
+// and we don't need to know we just use it.
+// similarly encapsulation mean's hiding things don't need to know.
---
+++ Programming with Javascript/modeule 3/abstaction.js
@@ -0,0 +1,2 @@
+// Abstraction is all about writing code in a way that will make it more generalized.
+
