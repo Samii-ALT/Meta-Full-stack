9be5cfa2b42af2d7afdd7b8b473d89995d47ae2a
Sami <aannss5602@gmail.com>
2026-08-15 15:33:03 +0500

    JS MOD3: Map updated

--- Programming with Javascript/modeule 3/map.js
+++ Programming with Javascript/modeule 3/map.js
@@ -4,3 +4,12 @@ var result = num.map(function(number){
     return number/10
 })
 console.log(result);
+
+
+
+let bestBoxers = new Map();
+bestBoxers.set(1, "The Champion");
+bestBoxers.set(2, "The Runner-up");
+bestBoxers.set(3, "The third place");
+
+console.log(bestBoxers);
