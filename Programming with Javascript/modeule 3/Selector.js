1d044aed3a786582e05235435bf5cf50ec3e0781
Sami <aannss5602@gmail.com>
2026-08-15 19:59:03 +0500

    JS MOD3: Selector Added

---
+++ Programming with Javascript/modeule 3/Selector.js
@@ -0,0 +1,22 @@
+// Write it in developer tools console section.
+
+document.querySelector('p')
+<p>
+"This domain is for use in documentation examples without needing
+permission. Avoid use in operations."
+</p>
+
+document.querySelector('a')
+<a href="https://iana. org/domains/example">Learn more</a>
+document.querySelectorAll('p')
+NodeList(2) [p, p] i
+0: p
+1: p
+length: 2
+[[Prototype]]: NodeList
+document.getElementById('heading')
+
+document.getElementsByClassName('txt')
+HTMLCollection [] i
+length: 0
+[[Prototype]]: HTMLCollection
