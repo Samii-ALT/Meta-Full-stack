0019fda15fbf6689564387cfdf0d1114e572e82e
Sami <aannss5602@gmail.com>
2026-08-15 19:35:35 +0500

    JS MOD3: DOM added

---
+++ Programming with Javascript/modeule 3/DOM.js
@@ -0,0 +1,15 @@
+// Write it in developer tools console section.
+
+const h2 = document. createElement('h2')
+
+h2.innerText = "This is an h2 heading"
+
+h2.setAttribute('id', 'sub-heading')
+
+h2.setAttribute('class', 'secondary')
+
+h2
+
+<h2 id="sub-heading" class="secondary">This is an h2 heading</h2>
+document.body.appendChild(h2)
+<h2 id="sub-heading" class="secondary">This is an h2 heading</h2>
