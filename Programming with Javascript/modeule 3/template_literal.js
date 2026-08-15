85ce1dc2ed79a883fd157ac107dbb0af2ad5bb1c
Sami <aannss5602@gmail.com>
2026-08-15 12:17:25 +0500

    JS MOD3: template_literal added

---
+++ Programming with Javascript/modeule 3/template_literal.js
@@ -0,0 +1,10 @@
+var greet = "Hello"
+var place = "World"
+console.log(`${greet} ${place}!`) // Hello World
+
+console.log(`${greet}   
+     ${place}
+        !`)
+
+console.log(`${1+1+1+1+1} Stars!`)    // 5 Stars!
+
