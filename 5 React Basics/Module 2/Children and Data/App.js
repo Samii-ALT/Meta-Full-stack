a7c431ce6b6b1c0a3cd7dc3232f1a258df7b4b99
Sami <aannss5602@gmail.com>
2026-08-27 16:01:23 +0500

    React-bsics MOD2: Children and data added

---
+++ 5 React Basics/Module 2/Children and Data/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Child from './Child';
+
+
+function App() {
+  const date = new Date();
+  return(
+    <Child message={date.toLocaleTimeString()} />
+  )
+}
+
+export default App;
