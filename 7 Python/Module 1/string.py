d5750b066e46b39422a7804794477abba6cbb951
Sami <aannss5602@gmail.com>
2026-09-15 19:38:31 +0500

    Python MOD1: String added

---
+++ 7 Python/Module 1/string.py
@@ -0,0 +1,22 @@
+para = "This is " \
+"a testing " \
+"paragrph"
+
+print(para)
+
+
+#name variable is assigned with the value of John
+name = 'John'
+print(name)
+# John
+#name is reassigned the value of Paul
+name = 'Paul'
+print(name)
+# Paul
+
+
+print(len(name))
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
