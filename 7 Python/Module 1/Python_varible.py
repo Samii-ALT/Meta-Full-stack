6f3609b953925f038b980371bc27236ff8045474
Sami <aannss5602@gmail.com>
2026-09-15 19:14:20 +0500

    Python MOD1: Python Variables added

---
+++ 7 Python/Module 1/Python_varible.py
@@ -0,0 +1,32 @@
+# Variable name should be meaninfgull..
+#  camelCase: ---   myName = Samiullah
+#  snake_case: ---  my_name = Samiullah
+
+x = 5
+print(x)
+
+y = "Sami"
+print(y)
+
+a=b=c=10
+
+print(a)
+print(b)
+print(c)
+
+
+a,b,c = 1,2,3
+
+print(a)
+print(b)
+print(c)
+
+
+
+s = "Sami"
+
+print(s);
+
+del s       # s variable is deleted..
+
+# print(s)  ---  This will crete an error bcz s is deleted to use it again we have to redefine
