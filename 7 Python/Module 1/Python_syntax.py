10e750d79233bf42931b31a2931af3018ed6bedb
Sami <aannss5602@gmail.com>
2026-09-15 18:56:33 +0500

    Python MOD1: Python Syntax added

---
+++ 7 Python/Module 1/Python_syntax.py
@@ -0,0 +1,40 @@
+# print("Hello") print("World")  =>  this is wrong way
+# there are two correct was to do this:
+
+# 1:
+print("Hello")
+print("World")
+
+# 2:
+print("Hello"); print("World")
+
+
+
+# ------------------------------------------------------
+
+
+
+
+x = 1 +      2
++3
+print(x)      # this will answer in 3 to solve use backslash "\"
+
+
+
+x = 1 +      2 \
++3
+print(x)
+
+
+
+# ---------------------------------------------
+
+
+# name = "John"
+# if name == "John":
+# print(name)                      This is wrong we need indentation afer ":"
+
+
+name = "John"
+if name == "John":
+    print(name)
