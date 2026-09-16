8b471776f6182d29dd805c12b001826b8ed5f770
Sami <aannss5602@gmail.com>
2026-09-16 15:42:48 +0500

    Python MOD1: Logical Operator added

---
+++ 7 Python/Module 1/logicalOperator.py
@@ -0,0 +1,17 @@
+a = True
+b = True
+
+if a and b:
+    print("Both A and B are True")
+
+a = True
+b = False
+
+if a or b:
+    print("True Or...")
+
+
+s = False
+if not s:
+    print("True not...")
+    
