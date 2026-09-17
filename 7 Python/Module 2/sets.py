eaf7e363ab7631649691a93ed2c4ee3dea989576
Sami <aannss5602@gmail.com>
2026-09-17 17:11:25 +0500

    Python MOD2: Sets added

---
+++ 7 Python/Module 2/sets.py
@@ -0,0 +1,7 @@
+set1 = {1, 2, 3, 4, 5}
+set2 = {4, 5, 6, 7, 8}
+
+set1.add(6)
+set1.remove(2)
+print(set1)
+print(set1.union(set2))  # Similarly we can do intersection, difference, symetric diff on sets.
