7482aa986b6e0e6a86aed56adbffd722c7aeea88
Sami <aannss5602@gmail.com>
2026-09-21 17:37:22 +0500

    Python MOD4: Import Statement added

---
+++ 7 Python/Module 4/import statement/ann/test2.py
@@ -0,0 +1,1 @@
+names = ["Sami", "LP_sami"]
---
+++ 7 Python/Module 4/import statement/main.py
@@ -0,0 +1,7 @@
+import test
+
+import sys
+sys.path.insert(1, r'C:\Users\aanns\Desktop\Meta_Full-Stack\7 Python\Module 4\import statement\ann')
+
+import test2
+print(test2.names)
